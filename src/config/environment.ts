c76d1cf6246bd51f4c3e21b95c584162f988bc06
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 14:55:21 -0500

    feat: Add jetSecret to environment configuration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,7 +3,9 @@ import 'dotenv/config'
 export const environment = {
     port: process.env.PORT || 3000,
     databaseURL: process.env.DATABASE_URL,
-    databaseName: process.env.DATABASE_NAME
+    databaseName: process.env.DATABASE_NAME,
+    jetSecret: process.env.JWT_SECRET,
+
 }
 
 
