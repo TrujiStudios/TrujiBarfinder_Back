8fb5c3f109ed5056aac3d638a8c79d3231521be0
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 11:33:10 -0500

    feat: Update authenticationController to remove unused getCompaniesController

--- src/controllers/authentication/authenticationController.ts
+++ src/controllers/authentication/authenticationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import { CompanyResponseDTO, CreateCompanyDTO } from '../../dtos/companyDTO';
-import createCompanyService, { getCompaniesService } from '../../services/authentication/autenticationService';
+import { CreateCompanyDTO } from '../../dtos/companyDTO';
+import createCompanyService from '../../services/authentication/autenticationService';
 
 
 
@@ -15,11 +15,3 @@ export const createCompanyContoller = async (req: Request, res: Response): Promi
     }
 };
 
-export const getCompaniesController = async (_req: Request, res: Response): Promise<Response<{ companies: CompanyResponseDTO[], count: number }>> => {
-    try {
-        const companies = await getCompaniesService();
-        return res.status(200).json(companies);
-    } catch (error: unknown) {
-        return res.status(500).json({ message: (error as Error).message });
-    }
-};
