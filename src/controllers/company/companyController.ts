4e5c0f71d6a17d43ff850df9e7f640b17055e4b9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 11:33:28 -0500

    feat: Implement getCompaniesController to retrieve list of companies

---
+++ src/controllers/company/companyController.ts
@@ -0,0 +1,16 @@
+
+import { Request, Response } from 'express';
+import { CompanyResponseDTO } from '../../dtos/companyDTO';
+import { getCompaniesService } from '../../services/company/companyServices';
+
+
+
+
+export const getCompaniesController = async (_req: Request, res: Response): Promise<Response<{ companies: CompanyResponseDTO[], count: number }>> => {
+    try {
+        const companies = await getCompaniesService();
+        return res.status(200).json(companies);
+    } catch (error: unknown) {
+        return res.status(500).json({ message: (error as Error).message });
+    }
+};
