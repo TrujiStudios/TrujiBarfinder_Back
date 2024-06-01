21be2067ad86ba4b2049d8ad7e15f0cc150465a7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-01 00:22:49 -0500

    feat: Add createProductController function
    The `createProductController` function in `productController.ts` has been added to handle the creation of products. This change allows for the creation of new products by accepting product data from the request body and calling the `createProductService` function. The function returns the newly created product as a JSON response with a status code of 201. In case of an error, a 500 status code is returned along with an error message. This enhancement improves the functionality of the application by providing a complete CRUD (Create, Read, Update, Delete) capability for products.

--- src/controllers/products/productController.ts
+++ src/controllers/products/productController.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express';
+import { CreateProductDTO } from '../../models/dtos/products/productDTO';
+import { createProductService } from '../../services/products/productService';
 
 
 
-export const getProductsController = async (_req: Request, res: Response): Promise<Response> => {
-    return res.status(200).json({ message: 'Hello World' });
+export const createProductController = async (_req: Request, res: Response) => {
+    const productData: CreateProductDTO = _req.body;
+    try {
+
+        const newProduct = await createProductService(productData);
+        return res.status(201).json(newProduct);
+
+    } catch (error: unknown) {
+        return res.status(500).json({ message: (error as Error).message })
+    }
 }
