2afa8d9747ea3dfaff86930b9e00ca5e58bf2449
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-01 09:56:00 -0500

    feat: Add getProductsController function
    The `getProductsController` function in `productController.ts` has been added to handle the retrieval of products. This change allows for fetching all products by calling the `getProductsService` function and returning them as a JSON response with a status code of 200. In case of an error, a 500 status code is returned along with an error message. This enhancement improves the functionality of the application by providing a complete CRUD (Create, Read, Update, Delete) capability for products.

--- src/controllers/products/productController.ts
+++ src/controllers/products/productController.ts
@@ -1,15 +1,29 @@
 import { Request, Response } from 'express';
 import { CreateProductDTO } from '../../models/dtos/products/productDTO';
-import { createProductService } from '../../services/products/productService';
+import { createProductService, getProductsService } from '../../services/products/productService';
 
 
 
-export const createProductController = async (_req: Request, res: Response) => {
+export const createProductController = async (_req: Request, res: Response): Promise<Response> => {
     const productData: CreateProductDTO = _req.body;
     try {
 
         const newProduct = await createProductService(productData);
         return res.status(201).json(newProduct);
+
+    } catch (error: unknown) {
+        return res.status(500).json({ message: (error as Error).message })
+    }
+}
+
+export const getProductsController = async (_req: Request, res: Response): Promise<Response> => {
+
+    const companyId: CreateProductDTO = _req.body;
+
+    try {
+
+        const products = await getProductsService(companyId);
+        return res.status(200).json(products);
 
     } catch (error: unknown) {
         return res.status(500).json({ message: (error as Error).message })
