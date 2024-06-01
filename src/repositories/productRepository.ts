0e19da686c01fbd9c69b6d1a1c913bb98bb5f730
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-01 11:47:32 -0500

    feat: Actualizar interfaz Product para utilizar ObjectId en lugar de Category

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -1,10 +1,10 @@
 import { CreateProductDTO, ProductResponseDTO } from "../models/dtos/products/productDTO";
 import { Product } from "../models/interfaces/products/productInterface";
 import db from "../config/database";
-import { Db } from 'mongodb';
+import { Db, ObjectId } from 'mongodb';
 
 
-export const createProductRepository = async (categoryData: CreateProductDTO): Promise<ProductResponseDTO> => {
+export const createProductRepository = async (productData: CreateProductDTO): Promise<ProductResponseDTO> => {
 
     const dbInstance: Db | null = await db;
     if (!dbInstance) {
@@ -13,7 +13,8 @@ export const createProductRepository = async (categoryData: CreateProductDTO): P
 
     const collection = dbInstance.collection<Product>('products');
     const resultCategory = await collection.insertOne({
-        ...categoryData,
+        ...productData,
+        category: new ObjectId(productData.category),
         status: true,
         createdAt: new Date(),
         updatedAt: new Date()
@@ -24,6 +25,19 @@ export const createProductRepository = async (categoryData: CreateProductDTO): P
     }
 
     return {} as ProductResponseDTO;
+    // return {
+    //     _id: resultCategory.insertedId.toString(),
+    //     name: productData.name,
+    //     description: productData.description,
+    //     price: productData.price,
+    //     category: productData.category,
+    //     company: productData.company,
+    //     status: productData.status,
+    //     image: productData.image,
+    //     code: productData.code,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    // };
 }
 
 
@@ -35,8 +49,63 @@ export const getProductsRepository = async (companyId: string): Promise<ProductR
         throw new Error('Database instance is null');
     }
 
-    const collection = dbInstance.collection<Product>('products');
-    const products = await collection.find({ companyId }).toArray();
+    const resultsPrducts = dbInstance.collection<Product>('products').aggregate(
+        [
+            {
+                $match: {
+                    company: companyId
+                }
+            },
+            {
+                $lookup: {
+                    from: "categories",     // collection name
+                    localField: "category",  //es el campo de la coleccion actual
+                    foreignField: "_id",
+                    as: "category"
+                }
+            },
+            {
+                $lookup: {
+                    from: "company",
+                    localField: "company",
+                    foreignField: "_id",
+                    as: "company"
+                }
+            },
+            {
+                $project: {
+                    name: 1,
+                    description: 1,
+                    price: 1,
+                    category: 1,
+                    company: 1,
+                    status: 1,
+                    image: 1,
+                    code: 1
+                }
+            }
+        ]
+    ).toArray();
 
-    return products;
+    // console.log(await resultsPrducts);
+    if (!resultsPrducts) {
+        throw new Error('Error getting products');
+    }
+
+    // return resultsPrducts as ProductResponseDTO[];
+
+
+    return (await resultsPrducts).map((product) => ({
+        _id: product._id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        category: product.category,
+        company: product.company,
+        status: product.status,
+        image: product.image,
+        code: product.code,
+        createdAt: product.createdAt, // Add createdAt property
+        updatedAt: product.updatedAt // Add updatedAt property
+    }));
 }
