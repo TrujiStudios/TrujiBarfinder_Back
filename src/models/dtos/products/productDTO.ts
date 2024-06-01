d5094af7a90e1027ef85870cd952db2d9479dff9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-01 00:21:00 -0500

    feat: Update productDTO interfaces
    The `productDTO.ts` file has been updated to include two new interfaces: `CreateProductDTO` and `ProductResponseDTO`. These interfaces define the structure of the product data for creating and retrieving products. This change improves the organization and clarity of the code by separating the different types of product data.

---
+++ src/models/dtos/products/productDTO.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../interfaces/products/categoryInterface";
+
+
+export interface CreateProductDTO {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    category: Category;
+    company: string;
+    status: boolean;
+    image: string;
+    code: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ProductResponseDTO {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    category: Category;
+    company: string;
+    status: boolean;
+    image: string;
+    code: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
