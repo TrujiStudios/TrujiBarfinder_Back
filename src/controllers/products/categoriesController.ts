91c9b478e448a425680c8b73885cd181ae64d38e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 21:29:23 -0500

    feat: Add getCategoriesController to retrieve list of categories

--- src/controllers/products/categoriesController.ts
+++ src/controllers/products/categoriesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createCategoryService } from '../../services/products/categoryService';
+import { createCategoryService, getCategoriesService } from '../../services/products/categoryService';
 import { CreateCategoryDTO } from '../../models/dtos/products/categoryDTO';
 
 
@@ -7,11 +7,32 @@ import { CreateCategoryDTO } from '../../models/dtos/products/categoryDTO';
 export const createCategoryController = async (_req: Request, res: Response): Promise<Response> => {
     const categoryData: CreateCategoryDTO = _req.body;
     try {
+        const companyId: string = _req.body.company;
+
+        console.log('Usuario autenticado:', companyId);
 
         const newCategory = await createCategoryService(categoryData);
         return res.status(201).json(newCategory);
 
     } catch (error: unknown) {
         return res.status(500).json({ message: (error as Error).message })
+    }
+}
+
+
+export const getCategoriesController = async (_req: Request, res: Response): Promise<Response> => {
+
+    try {
+
+        const companyId: string = _req.body.company;
+
+        const categories = await getCategoriesService(companyId);
+
+        return res.status(200).json(categories);
+
+
+    } catch (error: unknown) {
+        return res.status(500).json({ message: (error as Error).message })
+
     }
 }
