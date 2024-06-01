dbbee1ce58bf16e00a4e24ad286afbbf7abfe88c
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-01 00:22:35 -0500

    feat: Add createProductRepository function

---
+++ src/repositories/productRepository.ts
@@ -0,0 +1,27 @@
+import { CreateProductDTO, ProductResponseDTO } from "../models/dtos/products/productDTO";
+import { Product } from "../models/interfaces/products/productInterface";
+import db from "../config/database";
+import { Db } from 'mongodb';
+
+
+export const createProductRepository = async (categoryData: CreateProductDTO): Promise<ProductResponseDTO> => {
+
+    const dbInstance: Db | null = await db;
+    if (!dbInstance) {
+        throw new Error('Database instance is null');
+    }
+
+    const collection = dbInstance.collection<Product>('products');
+    const resultCategory = await collection.insertOne({
+        ...categoryData,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    });
+
+    if (resultCategory.acknowledged === false) {
+        throw new Error('Category was not created');
+    }
+
+    return {} as ProductResponseDTO;
+}
