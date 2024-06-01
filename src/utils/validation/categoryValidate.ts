d94c3d878f40d52c21315226d6f9b90fbc188576
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 21:30:23 -0500

    feat: Remove commented out code for companyId in categoryValidate.ts

--- src/utils/validation/categoryValidate.ts
+++ src/utils/validation/categoryValidate.ts
@@ -3,6 +3,6 @@ import Joi from 'joi';
 export const createCategorySchema = Joi.object({
     name: Joi.string().trim().min(3).max(30).required(),
     description: Joi.string().required().trim(),
-    companyId: Joi.string().required().trim(),
+    // companyId: Joi.string().required().trim(),
 
 });
