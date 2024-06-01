2975f82c95e26f8d682b2b5eb862b4894a05df49
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-31 21:30:34 -0500

    feat: Update createToken to use environment variable for secret key
    The `createToken` function in `createToken.ts` has been updated to use the `environment.jetSecret` variable as the secret key for signing the JWT token. This change enhances the security of the application by removing the hard-coded secret key and using a configurable value instead.

--- src/utils/createToken.ts
+++ src/utils/createToken.ts
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 import { Payload } from "../models/dtos/company/companyDTO";
-
+import { environment } from "../config/environment";
 
 
 
 
 
 export const createToken = (payload: Payload): string => {
-    return jwt.sign(payload, 'SECRET_KEY', { expiresIn: '1h' }); // El token expira en 1 hora
+    return jwt.sign(payload, environment.jetSecret || 'defaultSecret', { expiresIn: '1h' }); // El token expira en 1 hora
 };
