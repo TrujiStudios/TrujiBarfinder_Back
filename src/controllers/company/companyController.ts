7c31c8636d671d591173df3adf185a5a86eb7198
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-03 19:02:20 -0500

    chore: Actualizar controlador de inicio de sesión de empresa para incluir búsqueda por email y contraseña

--- src/controllers/company/companyController.ts
+++ src/controllers/company/companyController.ts
@@ -1,7 +1,7 @@
 
 import { Request, Response } from 'express';
 import { CompanyResponseDTO } from '../../models/dtos/company/companyDTO';
-import { getCompaniesService } from '../../services/company/companyServices';
+import { findCompanyByEmailServiceFixed, getCompaniesService } from '../../services/company/companyServices';
 
 
 
@@ -14,3 +14,20 @@ export const getCompaniesController = async (_req: Request, res: Response): Prom
         return res.status(500).json({ message: (error as Error).message });
     }
 };
+
+export const findCompanyByEmailControllerFixed = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        //body 
+        const { email } = req.body;
+        // const companyId: string = req.body.company;
+        const password: string = req.body.password;
+
+
+        const results = await findCompanyByEmailServiceFixed(email, password);
+        return res.status(200).json({
+            results
+        });
+    } catch (error: unknown) {
+        return res.status(500).json({ message: (error as Error).message });
+    }
+}
