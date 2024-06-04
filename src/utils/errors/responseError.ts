9251c7ef3a69f0e643c790381bb2f5fd7b745013
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-04 12:23:35 -0500

    feat: Add responseError utility function

---
+++ src/utils/errors/responseError.ts
@@ -0,0 +1,33 @@
+import { Response } from "express";
+
+interface ErrorWithCode {
+    code?: number;
+    message?: string;
+    errors?: string;
+    meta?: {
+        target?: Record<string, string>;
+    };
+}
+
+const errorResponse = (res: Response, error: ErrorWithCode) => {
+    if (error.hasOwnProperty("code") || error.hasOwnProperty("errors")) {
+
+        if (error.code === 401) {
+            return res.status(401).json({
+                ok: false,
+                errors: [{ message: error.message || error.errors }],
+            });
+        }
+
+        return res.status(400).json({
+            ok: false,
+            errors: [{ message: error.message || error.errors }],
+        });
+    }
+    return res.status(500).json({
+        ok: false,
+        errors: [{ message: error.message || error }],
+    });
+};
+
+export default errorResponse;
