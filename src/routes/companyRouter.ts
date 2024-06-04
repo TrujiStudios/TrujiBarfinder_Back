1390488d340dc0a055d8ee2786b748435b00dfd6
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-03 19:03:48 -0500

    feat: Add authentication middleware for companyRouter
    This commit adds the `authenticateToken` middleware to the `companyRouter` file, specifically to the `/listOne` route. This ensures that only authenticated users can access the route for retrieving a single company.

--- src/routes/companyRouter.ts
+++ src/routes/companyRouter.ts
@@ -1,6 +1,7 @@
 
 import express from 'express';
-import { getCompaniesController } from '../controllers/company/companyController';
+import { findCompanyByEmailControllerFixed, getCompaniesController } from '../controllers/company/companyController';
+import { authenticateToken } from '../middlewares/authentication/auth';
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ const router = express.Router();
 
 //Get company
 router.get('/list', getCompaniesController);
+
+//getOne company
+router.get('/listOne', authenticateToken, findCompanyByEmailControllerFixed);
 
 export default router;
