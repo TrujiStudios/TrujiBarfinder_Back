35fa71a5e9fdef031b45077c4cc4295411faf3a5
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-06-06 00:06:18 -0500

    feat: Update tablesController to include updateTablesController function

--- src/controllers/Tables/tablesController.ts
+++ src/controllers/Tables/tablesController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import { Unauthorized } from '../../utils/errors/errors';
 import errorResponse from '../../utils/errors/responseError';
-import { createTableService, getAllTablesService } from '../../services/table/table.Service';
-import { CreateTablesDTO } from '../../models/dtos/tables/tablesDTO';
+import { createTableService, getAllTablesService, updateTablesServices } from '../../services/table/table.Service';
+import { CreateTablesDTO, UpdateTablesDTO } from '../../models/dtos/tables/tablesDTO';
 
 export const createTablesController = async (_req: Request, res: Response): Promise<Response> => {
     const tableData: CreateTablesDTO = _req.body;
@@ -24,6 +24,24 @@ export const getAllTablesController = async (_req: Request, res: Response): Prom
             msg: 'All tables',
             message,
             data
+        });
+    } catch (error: unknown) {
+        return errorResponse(res, error as Error);
+    }
+}
+
+
+export const updateTablesController = async (_req: Request, res: Response): Promise<Response> => {
+    try {
+        if (!_req.session.isAutehnticated) throw new Unauthorized('Session not active');
+        const companyId: string = _req.body.company;
+        const tableId: string = _req.params.tableId;
+        const updatedData: Partial<UpdateTablesDTO> = _req.body;
+        const results = await updateTablesServices(companyId, tableId, updatedData);
+        return res.status(200).json({
+            msg: 'All tables',
+            // message: message,
+            data: results
         });
     } catch (error: unknown) {
         return errorResponse(res, error as Error);
