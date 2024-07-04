21893832a5cd4e268d537fb3c27cc17ad2938d53
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-07-03 19:00:07 -0500

    feat: Update createOrderRepository function to handle ObjectId conversion

--- src/repositories/orderRepository.ts
+++ src/repositories/orderRepository.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import db from "../config/database";
 import { CreateOrderDTO, OrderResponseDTO } from "../models/dtos/order/orderDTO";
 import { Order } from "../models/interfaces/order/orderInterface";
@@ -11,9 +12,18 @@ export const createOrderRepository = async (orderData: CreateOrderDTO): Promise<
         throw new Error('Database instance is null');
     }
 
+
     const collection = dbInstance.collection<Order>('orders');
     const resultOrder = await collection.insertOne({
         ...orderData,
+        tableId: new ObjectId(orderData.tableId),
+        userId: new ObjectId(orderData.userId),
+        // products: orderData.products.map(product => {
+        //     return {
+        //         productId: new ObjectId(product.productId),
+        //         quantity: product.quantity,
+        //         price: product.price
+        //     }
         status: 'pending',
         createdAt: new Date(),
         updatedAt: new Date()
