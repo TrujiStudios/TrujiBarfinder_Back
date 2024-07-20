973f24854831bcf8507dbe3a2ee56a825705b4c6
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-07-19 21:02:03 -0500

    feat: Update orderInterface to include OrderUpdate interface

--- src/models/interfaces/order/orderInterface.ts
+++ src/models/interfaces/order/orderInterface.ts
@@ -17,3 +17,20 @@ export interface Order {
         price: number;
     }>;
 }
+
+export interface OrderUpdate {
+    id?: string;
+    companyId?: string;
+    userId?: ObjectId;
+    tableId?: ObjectId;
+    status: string;
+    total: number;
+    createdAt: Date;
+    updatedAt: Date;
+    // eliminatedAt: Date;
+    products: Array<{
+        productId: ObjectId;
+        quantity: number;
+        price?: number;
+    }>;
+}
