b3d261a76ddbc8c163a6a60c15c7e1a98051e903
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-09-22 01:25:44 -0500

    feat: Actualizar modelo de datos de roles y permisos para incluir createdAt y updatedAt

--- src/repositories/roleRepository.ts
+++ src/repositories/roleRepository.ts
@@ -1,8 +1,8 @@
 import db from "../config/database";
-import { Db } from 'mongodb';
+import { Db, ObjectId } from 'mongodb';
 // import { BadRequest } from "../utils/errors/errors";
-import { RoleDTO, RoleResponseDTO } from "../models/dtos/role/roleDTO";
-import { Role } from "../models/interfaces/role/roleInteface";
+import { PermissionDTO, RoleDTO, RoleResponseDTO } from "../models/dtos/role/roleDTO";
+import { Permission, Role } from "../models/interfaces/role/roleInteface";
 
 export const createRoleRepository = async (roleData: RoleDTO): Promise<RoleResponseDTO> => {
 
@@ -11,28 +11,41 @@ export const createRoleRepository = async (roleData: RoleDTO): Promise<RoleRespo
         throw new Error('Database instance is null');
     }
 
-    // delete roleData._id;
+    // Buscar los permisos por sus IDs en la base de datos
+    const permissionDocs = await dbInstance.collection<Permission>('permissions')
+        .find({ _id: { $in: roleData.permissions.map(p => new ObjectId(p)) } })
+        .toArray();
 
-    const collection = dbInstance.collection<Role>('roles');
-    const resultRole = await collection.insertOne({
-        ...roleData,
-        // permissions: roleData.permissions.map(permission => ({
-        //     ...permission,
-        //     _id: new ObjectId()
-        // })),
+    // Validar que todos los permisos existen
+    if (permissionDocs.length !== roleData.permissions.length) {
+        throw new Error('Algunos permisos proporcionados no existen');
+    }
+
+    // Crear el nuevo rol con los permisos encontrados
+    const newRole = {
+        name: roleData.name,
+        permissions: permissionDocs, // Asigna los permisos encontrados
+        company: roleData.company,
         createdAt: new Date(),
         updatedAt: new Date()
-    });
+    };
+
+    // Insertar el rol en la base de datos
+    const collection = dbInstance.collection('roles');
+    const resultRole = await collection.insertOne(newRole);
 
     if (resultRole.acknowledged === false) {
-        throw new Error('Role was not created');
+        throw new Error('El rol no pudo ser creado');
     }
 
     return {
         _id: resultRole.insertedId.toString(),
         name: roleData.name,
         company: roleData.company,
-        // permissions: roleData.permissions,
+        permissions: permissionDocs.map(permission => ({
+            ...permission,
+            _id: permission._id.toString()
+        })), // Devolvemos los permisos completos
         createdAt: new Date(),
         updatedAt: new Date()
     };
@@ -56,12 +69,27 @@ export const getRoleRepository = async (
                 }
             },
             {
+                $lookup: {
+                    from: 'permissions',
+                    localField: 'permissions._id',
+                    foreignField: '_id',
+                    as: 'permissions'
+                }
+            },
+            {
                 $project: {
                     _id: 1,
                     name: 1,
                     company: 1,
                     createdAt: 1,
-                    updatedAt: 1
+                    updatedAt: 1,
+                    permissions: {
+                        _id: 1,
+                        name: 1,
+                        company: 1,
+                        createdAt: 1,
+                        updatedAt: 1
+                    }
                 }
             }
         ]
@@ -72,8 +100,47 @@ export const getRoleRepository = async (
         name: role.name,
         company: role.company,
         createdAt: role.createdAt,
-        updatedAt: role.updatedAt
+        updatedAt: role.updatedAt,
+        permissions: role.permissions.map((permission: { _id: { toString: () => any; }; name: any; company: any; createdAt: any; updatedAt: any; }) => ({
+            _id: permission._id.toString(),
+            name: permission.name,
+            company: permission.company,
+            createdAt: permission.createdAt,
+            updatedAt: permission.updatedAt
+        }))
     }));
 
 
+}
+
+export const createPermissionRepository = async (roleData: PermissionDTO): Promise<RoleResponseDTO> => {
+
+    const dbInstance: Db | null = await db;
+    if (!dbInstance) {
+        throw new Error('Database instance is null');
+    }
+
+    // delete roleData._id;
+
+    const collection = dbInstance.collection<Permission>('permissions');
+    const resultRole = await collection.insertOne({
+        ...roleData,
+        _id: new ObjectId(roleData._id),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        company: ""
+    });
+
+    if (resultRole.acknowledged === false) {
+        throw new Error('Role was not created');
+    }
+
+    return {
+        _id: resultRole.insertedId.toString(),
+        name: roleData.name,
+        company: "", // Add the company property
+        permissions: [], // Add the permissions property
+        createdAt: new Date(),
+        updatedAt: new Date()
+    };
 }
