9fd73a9f7eca9e709c6c342dfd6e50e11ee2602a
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-09-22 01:25:34 -0500

    feat: Actualizar modelo de datos de roles y permisos
    Se actualizaron los modelos de datos de roles y permisos para incluir los campos de createdAt y updatedAt. Además, se agregó el campo de company en el modelo de permisos. Esto permitirá un mejor seguimiento de la creación y actualización de roles y permisos en el sistema.

--- src/models/interfaces/role/roleInteface.ts
+++ src/models/interfaces/role/roleInteface.ts
@@ -4,14 +4,16 @@ import { ObjectId } from "mongodb";
 export interface Role {
     _id?: ObjectId;
     name: string;
-    // permissions: Permission[];
+    permissions: Permission[];
     createdAt: Date;
     updatedAt: Date;
     // eliminatedAt: Date;
 }
 
 export interface Permission {
-    _id: ObjectId;
+    _id?: ObjectId;
     name: string; // Ej: 'create_post', 'edit_post', 'delete_post'
-    description: string;  // Breve descripción de lo que permite el permiso
+    company: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
