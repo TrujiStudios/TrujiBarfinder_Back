36fb205cdea04073c7b1bc98a184c977940ef1e0
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-14 17:27:37 -0500

    feat: Agregar funciones para crear plantillas de roles de administrador y usuario en el repositorio de autenticación

--- src/repositories/roleRepository.ts
+++ src/repositories/roleRepository.ts
@@ -3,6 +3,150 @@ import { Db, ObjectId } from 'mongodb';
 // import { BadRequest } from "../utils/errors/errors";
 import { PermissionDTO, RoleDTO, RoleResponseDTO } from "../models/dtos/role/roleDTO";
 import { Permission, Role } from "../models/interfaces/role/roleInteface";
+// import plantillaRolAdmin from "../utils/plantillas/rol.admin";
+
+export const createPlantillaRolUserRepository = async (companyId: any): Promise<void> => {
+    const dbInstance: Db | null = await db;
+    if (!dbInstance) {
+        throw new Error('Database instance is null');
+    }
+    const collection = dbInstance.collection('roles');
+    const result = await collection.insertMany(
+        [
+            {
+                name: "planilla User",
+                type: "User",
+                defauldUser: true,
+                defauldAdmin: false,
+                authorization: {
+                    moduleApp: {
+                        accessModule: {
+                            showModule: true
+                        }
+                    },
+                    personalInformation: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        }
+                    },
+                    order: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        }
+                    }
+                },
+                accessTo: [],
+                company: new ObjectId(companyId),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]
+    );
+    if (result.acknowledged === false) {
+        throw new Error('PlantillaRolAdmin was not created');
+    }
+
+    return;
+};
+
+export const createPlantillaRolAdminRepository = async (companyId: any): Promise<void> => {
+    const dbInstance: Db | null = await db;
+    if (!dbInstance) {
+        throw new Error('Database instance is null');
+    }
+    const collection = dbInstance.collection('roles');
+    const result = await collection.insertMany(
+        [
+            {
+                name: "plantilla administrador",
+                type: "Admin",
+                defaulUser: false,
+                defalultAdmin: true,
+                authorizations: {
+                    moduleApp: {
+                        accessModule: {
+                            showModule: true
+                        }
+                    },
+                    users: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        },
+                        personalInformation: {
+                            access: {
+                                c: true,
+                                r: true,
+                                u: true,
+                                d: true
+                            }
+                        }
+                    },
+                    roles: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        }
+                    },
+                    table: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        }
+                    },
+                    order: {
+                        accessModule: {
+                            showModule: true
+                        },
+                        access: {
+                            c: true,
+                            r: true,
+                            u: true,
+                            d: true
+                        }
+                    }
+                },
+                accessTo: [],
+                company: new ObjectId(companyId),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]
+    );
+    if (result.acknowledged === false) {
+        throw new Error('PlantillaRolAdmin was not created');
+    }
+
+    return;
+};
+
 
 export const createRoleRepository = async (roleData: RoleDTO): Promise<RoleResponseDTO> => {
 
@@ -143,4 +287,92 @@ export const createPermissionRepository = async (roleData: PermissionDTO): Promi
         createdAt: new Date(),
         updatedAt: new Date()
     };
+}
+
+// accessModule
+export const accessModuleRepository = async (company: string, userId: string) => {
+
+    const dbInstance: Db | null = await db;
+    if (!dbInstance) {
+        throw new Error('Database instance is null');
+    }
+
+    //buscar el usuario en la base de datos por el ID y el roleId
+    const userResult = await dbInstance.collection('users').aggregate(
+        [
+            {
+                $match: {
+                    _id: new ObjectId(userId),
+                    company: new ObjectId(company),
+                }
+            },
+            {
+                $lookup: {
+                    from: 'roles',
+                    localField: 'roleId',
+                    foreignField: '_id',
+                    as: 'roles'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    company: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    roleId: 1,
+                    roles: {
+                        _id: 1,
+                        name: 1,
+                        type: 1,
+                        authorization: 1,
+                        company: 1,
+                        createdAt: 1,
+                        updatedAt: 1,
+                        permissions: 1
+                    }
+
+
+                }
+            }
+        ]
+    ).toArray();
+
+    if (userResult.length === 0) {
+        throw new Error('No se encontró el rol');
+    }
+    // console.log(roleResult[0].roles[0]._id);
+    //buscar el rol en la base de datos por el ID
+
+
+    // buscar el rol en la base de datos por el ID
+    // const roleResult = await dbInstance.collection('roles').find({ _id: new ObjectId(userResult[0].roles[0]._id) }).toArray();
+
+
+
+
+
+
+
+
+    return userResult.map((role): RoleResponseDTO => ({
+        _id: role._id,
+        name: role.name,
+        company: role.company,
+        createdAt: role.createdAt,
+        updatedAt: role.updatedAt,
+        permissions: role.roles.map((permission: { _id: { toString: () => any; }; name: any; type: any, authorization: string[], company: any; createdAt: any; updatedAt: any; }) => ({
+            _id: permission._id.toString(),
+            name: permission.name,
+            type: permission.type,
+            authorization: permission.authorization,
+            company: permission.company,
+            createdAt: permission.createdAt,
+            updatedAt: permission.updatedAt
+        }))
+    }))[0];
+
+
+
 }
