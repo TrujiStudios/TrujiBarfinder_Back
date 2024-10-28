daa4636e0323a4deacaa293938eebf95ac71a124
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-28 13:58:39 -0500

    Actualizar userRepository.ts: Crear y buscar usuarios en la base de datos

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,12 +1,9 @@
 import { Db, ObjectId } from "mongodb";
 import db from "../config/database";
-
 import { CreateUserDTO, UserResponseDTO } from "../models/dtos/user/userDTO";
 import { User } from "../models/interfaces/user/userInterface";
-// import { Role } from '../models/interfaces/role/roleInteface';
 
 export const createUserRepository = async (userData: CreateUserDTO): Promise<UserResponseDTO> => {
-
     const dbInstance: Db | null = await db;
     if (!dbInstance) {
         throw new Error('Database instance is null');
@@ -15,7 +12,6 @@ export const createUserRepository = async (userData: CreateUserDTO): Promise<Use
     const collection = dbInstance.collection<User>('users');
     const resultUser = await collection.insertOne({
         ...userData,
-        // id: userData.id!,
         roleId: new ObjectId(userData.roleId),
         status: true,
         createdAt: new Date(),
@@ -30,9 +26,98 @@ export const createUserRepository = async (userData: CreateUserDTO): Promise<Use
         id: resultUser.insertedId.toString(),
         _id: resultUser.insertedId.toString(),
         ...userData,
-        // companyId: '',
-        role: userData.roleId, // Assuming role is derived from roleId
+        role: {
+            _id: userData.roleId.toString(),
+            name: '', // Placeholder, should be populated from the role collection
+            type: '', // Placeholder, should be populated from the role collection
+            permissions: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
         createdAt: new Date(),
         updatedAt: new Date(),
     };
+}
+
+export const findUserByRepository = async (companyId: string): Promise<UserResponseDTO[]> => {
+
+    try {
+
+        const dbInstance: Db | null = await db;
+        if (!dbInstance) {
+            throw new Error('Database instance is null');
+        }
+
+        const resultsUser = await dbInstance.collection<User>('users').aggregate([
+            {
+                $match: {
+                    company: companyId,
+                }
+            },
+            {
+                $lookup: {
+                    from: 'roles',
+                    localField: 'roleId',
+                    foreignField: '_id',
+                    as: 'role',
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    lastName: 1,
+                    documentType: 1,
+                    typePerson: 1,
+                    email: 1,
+                    phone: 1,
+                    roleId: 1,
+                    status: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    role: {
+                        _id: 1,
+                        name: 1,
+                        type: 1,
+                        permissions: 1,
+                        createdAt: 1,
+                        updatedAt: 1,
+                    }
+                }
+            }
+        ]).toArray();
+
+        return resultsUser.map((user) => {
+            const role = user.role[0];
+            return {
+                id: user._id.toString(),
+                _id: user._id.toString(),
+                name: user.name,
+                lastName: user.lastName,
+                documentType: user.documentType,
+                typePerson: user.typePerson,
+                email: user.email,
+                phone: user.phone,
+                roleId: user.roleId.toString(),
+                status: user.status,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+                role: {
+                    _id: role._id?.toString(),
+                    name: role.name,
+                    type: role.type,
+                    permissions: role.permissions,
+                    createdAt: role.createdAt,
+                    updatedAt: role.updatedAt,
+                },
+                password: user.password // Assuming password is part of the user document
+            }
+        });
+
+    } catch (error: unknown) {
+        throw new Error('Error getting user: ' + (error as Error).message);
+
+    }
+
+
 }
