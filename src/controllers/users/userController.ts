a5cf44e85dae09452f9c256b3238686ebbfb2a67
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-10-28 13:58:01 -0500

    Actualizar controlador de usuarios en userController.ts

--- src/controllers/users/userController.ts
+++ src/controllers/users/userController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
-import { Unauthorized } from '../../utils/errors/errors';
+import { BadRequest, Unauthorized } from '../../utils/errors/errors';
 import errorResponse from '../../utils/errors/responseError';
-import { createUserService } from '../../services/user/userService';
+import { createUserService, findUserByService } from '../../services/user/userService';
 import { CreateUserDTO } from '../../models/dtos/user/userDTO';
+import { accessModuleService } from '../../services/role/roleService';
 
 
 export const createUserController = async (_req: Request, res: Response): Promise<Response> => {
@@ -11,6 +12,40 @@ export const createUserController = async (_req: Request, res: Response): Promis
         if (!_req.session.isAutehnticated) throw new Unauthorized('Session not active');
         const resultTable = await createUserService(tableData);
         return res.status(201).json(resultTable);
+    } catch (error: unknown) {
+        return errorResponse(res, error as Error);
+    }
+}
+
+//get user
+export const findUserByController = async (_req: Request, res: Response): Promise<Response> => {
+    const companyId: string = _req.body.company;
+    const sessionUser = _req.session?.user;
+    const sessionCompany = _req.session?.company;
+
+    try {
+        if (!_req.session?.isAutehnticated) throw new Unauthorized('Session not active');
+
+        if (sessionUser) {
+            const userId = sessionUser._id;
+            if (typeof userId !== 'string') throw new BadRequest('Invalid user ID');
+            const module = 'users';
+            const accessResponse = await accessModuleService(companyId, userId, module);
+            if (!accessResponse.permissions.read) {
+                throw new BadRequest('User does not have read access to this module');
+            }
+        }
+        if (sessionCompany) {
+            const companyId = sessionCompany._id;
+            if (typeof companyId !== 'string') throw new BadRequest('Invalid company ID');
+            const module = 'users';
+            const accessResponse = await accessModuleService(companyId, companyId, module);
+            if (!accessResponse.permissions.read) {
+                throw new BadRequest('User does not have read access to this module');
+            }
+        }
+        const resultsUser = await findUserByService(companyId);
+        return res.status(200).json(resultsUser);
     } catch (error: unknown) {
         return errorResponse(res, error as Error);
     }
