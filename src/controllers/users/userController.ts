66cc5216087fd915372afe2f707d63d240f644ef
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-11-11 18:50:03 -0500

    Actualizar userController.ts: Agregar validaciones de acceso y autenticación para la creación de usuarios

--- src/controllers/users/userController.ts
+++ src/controllers/users/userController.ts
@@ -8,8 +8,30 @@ import { accessModuleService } from '../../services/role/roleService';
 
 export const createUserController = async (_req: Request, res: Response): Promise<Response> => {
     const tableData: CreateUserDTO = _req.body;
+    const companyId: string = _req.body.company;
+    const sessionUser = _req.session?.user;
+    const sessionCompany = _req.session?.company;
     try {
-        if (!_req.session.isAutehnticated) throw new Unauthorized('Session not active');
+        if (!_req.session?.isAutehnticated) throw new Unauthorized('Session not active');
+
+        if (sessionUser) {
+            const userId = sessionUser._id;
+            if (typeof userId !== 'string') throw new BadRequest('Invalid user ID');
+            const module = 'users';
+            const accessResponse = await accessModuleService(companyId, userId, module);
+            if (!accessResponse.permissions.create) {
+                throw new BadRequest('User does not have read access to this module');
+            }
+        }
+        if (sessionCompany) {
+            const companyId = sessionCompany._id;
+            if (typeof companyId !== 'string') throw new BadRequest('Invalid company ID');
+            const module = 'users';
+            const accessResponse = await accessModuleService(companyId, companyId, module);
+            if (!accessResponse.permissions.create) {
+                throw new BadRequest('User does not have read access to this module');
+            }
+        }
         const resultTable = await createUserService(tableData);
         return res.status(201).json(resultTable);
     } catch (error: unknown) {
